108a9ccdb80ca65abfb2ef4760fb7fcb7a242213
Julio Robledo <Jrobledo94@hotmail.com>
2023-08-15 08:24:00 -0600

    si

--- seguimiento_ciudadano/views.py
+++ seguimiento_ciudadano/views.py
@@ -7,18 +7,16 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.db.models import F
-from .models import Question, Choice
+# from .models import Question, Choice
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 
 
+
 class IndexView(generic.ListView):
     template_name = "seguimiento_ciudadano/index.html"
-    context_object_name = "latest_question_list"
-    def get_queryset(self) -> QuerySet[Any]:
-        return Question.objects.order_by("-pub_date")[:5]
-
+    context_object_name = "IndexSeguimiento"
 
 
 def signin(request):
@@ -28,6 +26,6 @@ def signin(request):
         user = authenticate(
             request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'polls/login.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
+            return render(request, 'seguimiento_ciudadano/login.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
         login(request, user)
         return redirect('seguimiento_ciudadano:vote')
