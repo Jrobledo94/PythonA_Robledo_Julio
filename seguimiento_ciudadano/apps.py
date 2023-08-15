be0448e2b4be5cb70cec4dd76eb493896ff043a4
Julio Robledo <Julio.Robledo@chihuahua.com.mx>
2023-08-15 00:13:25 -0600

    seguimiento_ciudadano siuuuu

---
+++ seguimiento_ciudadano/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SeguimientoCiudadanoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'seguimiento_ciudadano'
---
+++ seguimiento_ciudadano/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+
+# Create your views here.
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.db.models import F
+from .models import Question, Choice
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+
+
+class IndexView(generic.ListView):
+    template_name = "seguimiento_ciudadano/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self) -> QuerySet[Any]:
+        return Question.objects.order_by("-pub_date")[:5]
+
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'seguimiento_ciudadano/login.html', {"form": AuthenticationForm})
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'polls/login.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
+        login(request, user)
+        return redirect('seguimiento_ciudadano:vote')
